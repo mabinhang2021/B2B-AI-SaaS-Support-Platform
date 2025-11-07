0fa735762d628f919bfee00214842fcec3a97335
mabinhang2021 <1223841837@qq.com>
2025-11-07 15:21:48 +0800

    10:dashboard index

---
+++ apps/web/modules/dashboard/ui/layouts/conversations-layout.tsx
@@ -0,0 +1,27 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@workspace/ui/components/resizable';
+import { ConverSationsPanel } from '../components/conversations-panel';
+
+export const ConversationsLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ResizablePanelGroup
+      className="h-full flex-1"
+      direction="horizontal"
+    >
+        <ResizablePanel defaultSize={30} maxSize={30} minSize={20}>
+            <ConverSationsPanel />
+        </ResizablePanel>
+        <ResizableHandle/>
+        <ResizablePanel className="h-full" defaultSize={70}  >
+            {children}
+        </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
